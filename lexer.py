051b970ef4878ce29ba8178058c3808afa1fbe96
Bartlomiej Kozka <bartek.kozka15@gmail.com>
2024-10-08 11:11:07 +0200

    add some code to lexer.py

--- lexer.py
+++ lexer.py
@@ -17,37 +17,46 @@ class lexer:
             case "=":
                 if self.getNextChar() == "=": 
                     token = Token(tokenKind=Token.EQEQ)
-                    self.nextChar()
+                    self.next2Char()
                 else:                         
-                    token =  Token(tokenKind=Token.EQEQ)
+                    token =  Token(tokenKind=Token.EQ)
+                    self.nextChar()
             case ">":
                 if self.getNextChar() == "=": 
                     token = Token(tokenKind=Token.GTEQ)
-                    self.nextChar()
+                    self.next2Char()
                 else:                         
                     token = Token(tokenKind=Token.GT)
+                    self.nextChar()
             case "<":
                 if self.getNextChar() == "=": 
                     token = Token(tokenKind=Token.LTEQ)
-                    self.nextChar()
+                    self.next2Char()
                 else:                         
                     token = Token(tokenKind=Token.LT)
+                    self.nextChar()
             case "!":
                 if self.getNextChar() == "=": 
                     token = Token(tokenKind=Token.NOTEQ)
-                    self.nextChar()
+                    self.next2Char()
                 else:                         
-                    raise Exception("Wrong syntax!")
+                    raise Exception("Wrong syntax!")            #temporary resolution !!!
             case "*":
                 token = Token(tokenKind=Token.ASTERISK)
+                self.nextChar()
             case "/":
                 token = Token(tokenKind=Token.SLASH)
+                self.nextChar()
             case "+":
                 token = Token(tokenKind=Token.PLUS)
+                self.nextChar()
             case "-":
                 token = Token(tokenKind=Token.MINUS)
+                self.nextChar()
             case "\\":
-                if self.getNextChar() == "n":   token = Token(tokenKind=Token.NEWLINE)
+                if self.getNextChar() == "n":   
+                    token = Token(tokenKind=Token.NEWLINE)
+                    
                 elif self.getNextChar() == "0": token = Token(tokenKind=Token.EOF)
                 else:                           raise Exception("Wrong syntax!")
             case "\"":
@@ -60,19 +69,30 @@ class lexer:
                 startIdx = self.currIdx
                 while self.currChar.isalpha() is True:
                     self.nextChar()
-                token = Token(self.source[startIdx : self.currIdx], Token.IDENT)
+                temp = self.source[startIdx:self.currIdx]
+                match temp:
+                    case Token.GOTO.__name__: token = Token(self.source[startIdx:self.currIdx], Token.GOTO)
+                    case Token.END.__name__:  token = Token(self.source[startIdx:self.currIdx], Token.END)
+                    case Token.IF.__name__:   token = Token(self.source[startIdx:self.currIdx], Token.IF)
+                    case Token.SQRT.__name__: token = Token(self.source[startIdx:self.currIdx], Token.SQRT)
+                    case Token.THEN.__name__: token = Token(self.source[startIdx:self.currIdx], Token.THEN)
+                    case _:                   token = Token(self.source[startIdx : self.currIdx], Token.IDENT)
             case _ if self.currChar.isnumeric() is True:
                 startIdx = self.currIdx
                 while self.currChar.isnumeric() is True or (self.currChar == "." and self.getNextChar().isnumeric() is True):
-                    
+                    self.nextChar()
+                token = Token(self.source[startIdx:self.currIdx], Token.NUMBER)
 
-            
         self.nextChar()
 
             
 
     def nextChar(self):
         self.currIdx += 1
+        self.currChar = self.source[self.currIdx]
+    
+    def next2Char(self):
+        self.currIdx += 2
         self.currChar = self.source[self.currIdx]
 
     def getNextChar(self): 
