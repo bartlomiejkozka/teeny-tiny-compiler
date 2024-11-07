49b44c398b76f34e193dbb26becfbb66dfea2d22
Bartlomiej Kozka <bartek.kozka15@gmail.com>
2024-11-07 10:23:31 +0100

    working on user authentication

---
+++ backend/database.py
@@ -0,0 +1,67 @@
+from typing import List 
+from typing import Optional
+from sqlalchemy import String, ForeignKey
+from sqlalchemy.orm import Mapped
+from sqlalchemy.orm import mapped_column
+from sqlalchemy.orm import relationship
+from sqlalchemy.orm import declarative_base
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy import create_engine
+
+Base = declarative_base()
+
+class User(Base):
+    __tablename__ = 'users'
+
+    id:         Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
+    name:       Mapped[str] = mapped_column(String(30))
+    login:       Mapped[str] = mapped_column(String(30))
+    password:   Mapped[str] = mapped_column(String(50))
+
+    scripts: Mapped[List['Script']] = relationship(back_populates='user')
+
+    def __repr__(self):
+        return f"User(id={self.id!r}, name={self.name!r}, login={self.login!r})"
+
+
+class Script(Base):
+    __tablename__ = 'scripts'
+
+    id:             Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
+    pesudo_code:    Mapped[str] = mapped_column(String(10000))
+    user_id:        Mapped[int] = mapped_column(ForeignKey('users.id'))
+
+    user: Mapped[User] = relationship(back_populates='scripts')
+
+    def __repr__(self):
+        return f"Script(id={self.id!r}, pesudo_code={self.pesudo_code!r}, user_id={self.user_id!r})"
+    
+
+class SessionToken(Base):
+    __tablename__ = 'session_tokens'
+
+    token:      Mapped[str] = mapped_column(String, primary_key=True)
+    user_id:    Mapped[int] = mapped_column(ForeignKey('users.id'))
+
+    user: Mapped[User] = relationship(back_populates='session_tokens')
+
+    def __repr__(self):
+        return f"SessionToken(id={self.id!r}, token={self.token!r}, user_id={self.user_id!r})"
+
+
+
+class Database:
+    def __init__(self, user, password, host, port, database):
+        self.database_url = "mysql+pymysql://" + user + ":" + password + "@" + host + ":" + port + "/" + database
+        self.engine = create_engine(self.database_url)
+        self.SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=self.engine)
+
+    def create_tables(self):
+        Base.metadata.create_all(bind=self.engine)
+
+    def get_session(self):
+        db = self.SessionLocal()
+        try:
+            yield db
+        finally:
+            db.close()
--- backend/main.py
+++ backend/main.py
@@ -1,8 +1,9 @@
 from lexer import *
 from parser import *  
+from database import *
+
 import sys
 import subprocess
-
 from fastapi import FastAPI
 from pydantic import BaseModel
 from fastapi.middleware.cors import CORSMiddleware
@@ -10,6 +11,14 @@ import uvicorn
 from contextlib import asynccontextmanager
 import webbrowser
 import asyncio
+from fastapi import Depends
+from sqlalchemy.orm import Session
+from fastapi import HTTPException
+import uuid
+from fastapi.responses import JSONResponse
+from fastapi.requests import Request
+from fastapi.responses import RedirectResponse
+from fastapi.templating import Jinja2Templates
 
 
 def compileCcode(sourceFile):
@@ -28,6 +37,8 @@ def start_frontend():
 
 
 app = FastAPI()
+database = Database()        # CHANGE
+database.create_tables()
 
 origins = [ "http://localhost:8888" ]
 
@@ -38,6 +49,26 @@ app.add_middleware(
     allow_methods=["*"],
     allow_headers=["*"] 
 )
+
+# Authentication user functions:
+def isUserYet(user: User, db: Session = Depends(database.get_session)):
+    return db.query(User).filter(User.login == user.login).first()
+
+def isLoginPasswordCorrect(user: User, db: Session = Depends(database.get_session)):
+    return db.query(User).filter(User.login == user.login).filter(User.password == user.password).first()
+
+def get_current_user(request: Request):
+    session_token = request.cookies.get("session_token")
+    if session_token is None:
+        raise HTTPException(status_code=401, detail="Not authenticated")
+    
+    session = database.get_session()
+    user_id = session.query(SessionToken).filter(SessionToken.token == session_token).first().user_id
+    if user_id is None:
+        raise HTTPException(status_code=401, detail="Not authenticated01")
+    
+    return {"user_id": user_id}
+
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
@@ -57,6 +88,43 @@ app.router.lifespan_context = lifespan
 
 class Code(BaseModel):
     code: str
+class CodeName(BaseModel):
+    name: str
+class User(BaseModel):
+    name: str
+    login: str
+    password: str
+
+
+@app.post("/signup")
+def register(user: User, db: Session = Depends(database.get_session)):
+    if isUserYet(user) is not None:
+        raise HTTPException(status_code=400, detail="User already exists!")
+    db.add(User(name=user.name, login=user.login, password=user.password))
+    db.commit()
+    return {"user": "User registered successfully!"}
+
+
+@app.post("/signin")
+def login(user: User, db: Session = Depends(database.get_session)):
+    if isLoginPasswordCorrect(user) is None:
+        raise HTTPException(status_code=400, detail="Incorrect login or password!")
+    
+    session_token = str(uuid.uuid4())
+    db.add(SessionToken(token=session_token, user_id=user.id))
+    db.commit()
+    #response = JSONResponse(content={"message": "Logged in successfully!"})
+    response = RedirectResponse(url="/")
+    response.set_cookie(key="session_token", value=session_token, httponly=True)
+    return response
+
+
+@app.get("/")
+def read_root(request: Request, user: dict = Depends(get_current_user)):
+    #return templates.TemplateResponse("home.html", {"request": request})
+    
+    
+
 
 @app.post("/run_code")
 def run_code(source: Code):
@@ -66,6 +134,16 @@ def run_code(source: Code):
     p.program()
     p.emitter.writeFile()
     return {"code": compileCcode("output.c")}
+
+# authentication required
+@app.post("/save_code")
+def save_code(source: Code, source_name: CodeName, db: Session = Depends(database.get_session), user: dict = Depends(get_current_user)):
+    source_code = source.code
+    db.add(Script(pesudo_code=source_code, user_id=user["user_id"]))
+    db.commit()
+    return {"code": "Code saved successfully!"}
+
+
 
 
 if __name__ == "__main__":
